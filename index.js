8e9338e31311eff49dce05f7a7d5338bec973b46
Minli Chen <cml_hawke0@163.com>
2015-07-16 00:24:30 +0800

    1

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ var Crawler = require('./lib/crawler.js');
 var crawler = new Crawler('cml_hawke0@163.com', 'kaze131021', './cookies.json');
 crawler.login();
 //crawler.crawl('http://www.zhihu.com/people/kaze13/followees');
-//crawler.getMoreFollowess();
+crawler.getMoreFollowess();
