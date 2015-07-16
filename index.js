2c0a0589f1e1c530bb0d058566a572f1685e6595
Rey <chen_m@worksap.co.jp>
2015-07-16 18:30:09 +0800

    1

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var Crawler = require('./lib/crawler.js');
 
-var crawler = new Crawler('cml_hawke0@163.com', 'kaze131021', './cookies.json');
+var crawler = new Crawler('', '', './cookies.json');
 crawler.login();
 //crawler.crawl('http://www.zhihu.com/people/kaze13/followees');
 crawler.getMoreFollowess();
