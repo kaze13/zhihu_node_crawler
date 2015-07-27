b0a6b5fccaa5bee0dcfcd5330bad8d32a65d6034
Rey <chen_m@worksap.co.jp>
2015-07-27 14:28:56 +0800

    change callback to promise

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ var Crawler = require('./lib/crawler.js');
 var crawler = new Crawler('', '', './cookies.json');
 crawler.login();
 //crawler.crawl('http://www.zhihu.com/people/kaze13/followees');
-crawler.getMoreFollowess();
+//crawler.getMoreFollowess();
